ddbca52737ca0ba020f62296d25597837bb2558c
angela <abrennec@gmail.com>
2020-06-08 14:55:50 +0200

    adding shaders

---
+++ 06_meshes_shaders/code/05_screen_grab_tex_shader/bin/data/shaders/mask_vert.glsl
@@ -0,0 +1,23 @@
+
+#version 120
+
+// Introduce some newer features to the older GLSL version.
+#extension GL_ARB_texture_rectangle : enable
+#extension GL_EXT_gpu_shader4 : enable
+
+
+
+void main()
+{
+    // Calculate the projected position in clip space with built-in function.
+    gl_Position = ftransform();
+    
+
+    // Set the texture coordinates by specifying the built-in vertex
+    // attribute gl_TexCoord[0] with the texture coordinate of the
+    // bound texture. By default, the texture coordinate of the bound
+    // texture is held in the built-in variable gl_MultiTexCoord0.
+    // The texture coordinates are interpolated per pixel based on the
+    // initally specified texture coordinates held in gl_MultiTexCoord0.
+    gl_TexCoord[0] = gl_MultiTexCoord0;
+}
